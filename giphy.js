c51350cc9fd57b763766b487b505762429136941
DanielFMurillo <danielfmurillo@yahoo.com>
2017-05-16 21:28:23 -0700

    updated code

--- giphy.js
+++ giphy.js
@@ -5,18 +5,36 @@ $(document).ready(function(){
 // create an array of topics
 var movies = ["The Hangover","Step Brothers", "Dumb and Dumber", "White Chicks", "Superbad", "21 Jump Street"];
 
-	for ( i = 0; i < movies.length; i++){
-		    var buttons =	$("<button>");
+	for (i = 0; i < movies.length; i++){
+		    var buttons =	$("<button>")
+        var buttonsDiv = $("#buttonsDiv");
+        buttons.text(movies[i]);
+        buttonsDiv.append(buttons);
+        buttons.attr("data-movie", movies[i]);
+        buttons.attr("data-state", "still");
+        buttons.attr("data-state", "animate");
 
 
-// 	}
+};
+
+$("#add").on("click", function(){
+    var input = $("#input").val();
+    // var buttons = $(<"button>")
+        buttons.text(input);
+        $("#buttonsDiv").append(buttons);
+
+});
+
 
 
 
 $("button").on("click", function() {
-		var movie = $(this).attr("movie-title");
+		var movie = $(this).attr("data-movie");
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC";		
 		// console.log(this);
+
+//clear the divs before the ajax call
+$("#gifs").empty(); 
 		
 $.ajax({
         url: queryURL,
@@ -34,15 +52,38 @@ $.ajax({
            	var p = $("<p>").text("Rating: " + rating);
            	var image = $("<img>");
            		image.attr("src", results[i].images.original_still.url);
+              image.attr("data-still", results[i].images.original_still.url);
+              image.attr("data-animate", results[i].images.fixed_height.url);
            		gifDiv.prepend(p);
            		gifDiv.prepend(image);
 
            		$("#gifs").prepend(gifDiv);
+
       		}
 
       	});
 
       }); 
+
+
+
+$("#gifs").on("click", "img" ,function(){
+        var state = $(this).attr("data-state");
+        console.log($(this));
+
+        if(state === "still"){
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+       } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+
+
+        }
+
+});
+
+
 
 // $("#item").on("click",function(){
 
